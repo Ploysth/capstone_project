3bd94ea26014e8c05f364bd60996aaa1ffd18835
Ploy <ploy-sukhapath@outlook.com>
2022-05-02 19:46:30 +0200

    update app.ja

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import React, { useState } from "react";
 import CheckInCardList from "./CheckInCardList";
+import BoardingCardList from "./BoardingCardList";
 
 function App() {
   const [cards, setCards] = useState(taskCards);
+  const [boardingCards, setboardingCards] = useState(taskBoarding);
   return (
     <>
       <CheckInCardList cards={cards} />
+      <BoardingCardList boardingCards={boardingCards} />
     </>
   );
 }
@@ -25,6 +28,18 @@ const taskCards = [
   },
 ];
 
-
+const taskBoarding = [
+  {
+    id: 1,
+    title: "Boarding",
+    timestart: "Uhrzeit von: 03:50",
+    timeend: "Uhrzeit bis: 04:05",
+    airline: "Airline: XQ",
+    flightnumber: "Flugnummer: 963",
+    destination: "destination: ADB",
+    registration: "Registration: TCSPB",
+    gate: "Gate: 337",
+  },
+];
 
 export default App;
