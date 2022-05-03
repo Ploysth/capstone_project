dada42378449a5bd8feaea4247f4b6793fedbf48
Ploy <ploy-sukhapath@outlook.com>
2022-05-03 15:21:11 +0200

    add input

--- src/FormBoarding.js
+++ src/FormBoarding.js
@@ -1,43 +1,54 @@
-import React from 'react'
+import React from "react";
 
-export default function FormBoarding() {
+export default function FormBoarding({ values }) {
   return (
     <div className="form-container">
-        <h1>Add new Boarding</h1>
+      <h1>Add new Boarding</h1>
       <form className="checkin-form">
         <input
+          value={values.timeStart}
           className="form-field"
           placeholder="Uhrzeit von"
           name="timeStart"
         />
 
         <input
+          value={values.timeEnd}
           className="form-field"
           placeholder="Uhrzeit bis"
           name="timeEnd"
         />
 
-        <input className="form-field" placeholder="Airline" name="airline" />
+        <input
+          value={values.airline}
+          className="form-field"
+          placeholder="Airline"
+          name="airline"
+        />
 
         <input
+          value={values.flightNumber}
           className="form-field"
           placeholder="Flugnummer"
           name="flightnumber"
         />
 
         <input
+          value={values.destination}
           className="form-field"
           placeholder="Destination"
           name="destination"
         />
 
         <input
+          value={values.registration}
           className="form-field"
           placeholder="Registration"
           name="registration"
         />
 
         <input
+          value={values.gate}
           className="form-field"
           placeholder="Gate"
           name="gate"
