011c8210b30be1240bed9cc44e764c5486fd36c2
Ploy <ploy-sukhapath@outlook.com>
2022-05-03 15:21:18 +0200

    add input

--- src/FormBreak.js
+++ src/FormBreak.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
-export default function FormBreak() {
+export default function FormBreak({ values }) {
   return (
     <div className="form-container">
-        <h1>Add Break</h1>
+      <h1>Add Break</h1>
       <form className="checkin-form">
         <input
+          value={values.timeStart}
           className="form-field"
           placeholder="Uhrzeit von"
           name="timeStart"
         />
 
         <input
+          value={values.timeEnd}
           className="form-field"
           placeholder="Uhrzeit bis"
           name="timeEnd"
         />
-
-        
       </form>
     </div>
   );
