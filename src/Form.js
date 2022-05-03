5bfed8f2e3697572c72d963baa36217f4c7cd384
Ploy <ploy-sukhapath@outlook.com>
2022-05-03 15:20:50 +0200

    add input to Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,43 +1,54 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({ values }) {
   return (
     <div className="form-container">
-        <h1>Add new Check In</h1>
+      <h1>Add new Check In</h1>
       <form className="checkin-form">
         <input
+          value={values.timeStart}
           className="form-field"
           placeholder="Uhrzeit von"
           name="timeStart"
         />
 
         <input
+          value={values.timeEnd}
           className="form-field"
           placeholder="Uhrzeit bis"
           name="timeEnd"
         />
 
-        <input className="form-field" placeholder="Airline" name="airline" />
-
         <input
+          value={values.airline}
           className="form-field"
-          placeholder="Flugnummer"
-          name="flightnumber"
+          placeholder="Airline"
+          name="airline"
         />
 
         <input
+          value={values.flightNumber}
+          className="form-field"
+          placeholder="Flugnummer"
+          name="flightNumber"
+        />
+
+        <input
+          value={values.destination}
           className="form-field"
           placeholder="Destination"
           name="destination"
         />
 
         <input
+          value={values.registration}
           className="form-field"
           placeholder="Registration"
           name="registration"
         />
 
         <input
+          value={values.checkin}
           className="form-field"
           placeholder="Check In Counter"
           name="checkin"
